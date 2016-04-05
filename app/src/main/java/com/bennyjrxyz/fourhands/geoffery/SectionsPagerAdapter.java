1256c7657182611a62c2c9221f1fb54ce3f384ca
jrbenny35 <bennyjr169@gmail.com>
2016-04-04 20:43:03 -0400

    Initial commit adding files for project

---
+++ app/src/main/java/com/bennyjrxyz/fourhands/geoffery/SectionsPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.bennyjrxyz.fourhands.geoffery;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by bennyjr on 4/4/16.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 6;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+            case 2:
+                return "SECTION 3";
+            case 3:
+                return "SECTION 4";
+            case 4:
+                return "SECTION 5";
+            case 5:
+                return "SECTION 6";
+        }
+        return null;
+    }
+}
